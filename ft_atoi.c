dc9246c27c97567a61f1093a97eae34abd0aea46
sanghunka <sanghunka@gmail.com>
2021-12-05 21:56:36 +0900

    Fix to handle long long overflow&underflow

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,12 @@
 /*   By: sankang <sankang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 16:28:03 by sankang           #+#    #+#             */
-/*   Updated: 2021/12/04 23:18:37 by sankang          ###   ########.fr       */
+/*   Updated: 2021/12/05 21:54:59 by sankang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
 static int	__isspace(int c)
 {
@@ -19,9 +20,10 @@ static int	__isspace(int c)
 
 int	ft_atoi(const char *str)
 {
-	const unsigned long long	ullmax = 9223372036854775807ULL;
-	const unsigned long long	ullmin = 9223372036854775808ULL;
+	const unsigned long long	ullmax = LLONG_MAX;
+	const unsigned long long	ullmin = -1 * (ullmax + 1);
 	unsigned long long			n;
+	unsigned long long			n_before;
 	int							sign;
 
 	n = 0;
@@ -33,10 +35,11 @@ int	ft_atoi(const char *str)
 			sign = -1;
 	while (ft_isdigit(*str))
 	{
+		n_before = n;
 		n = 10 * n + (*str++ - '0');
-		if ((n > ullmin) && (sign == -1))
+		if (((n > ullmin) || (n < n_before)) && (sign == -1))
 			return (0);
-		if ((n > ullmax) && (sign == 1))
+		if (((n > ullmax) || (n < n_before)) && (sign == 1))
 			return (-1);
 	}
 	return (sign * n);
